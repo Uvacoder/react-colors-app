ae94fdcab8568a041d54d17e4a14294842191827
Nikki <npeel3497@gmail.com>
2020-02-16 14:49:07 -0500

    Add SavePalette

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -86,6 +86,7 @@ class NewPaletteForm extends Component {
         this.updateCurrentColor = this.updateCurrentColor.bind(this);
         this.addNewColor = this.addNewColor.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     componentDidMount() {
         ValidatorForm.addValidationRule('isColorNameUnique', value => 
@@ -123,6 +124,17 @@ class NewPaletteForm extends Component {
         this.setState({ newName: evt.target.value });
     }
 
+    handleSubmit() {
+        let newName = "New Test Palette";
+        const newPalette = {
+            paletteName: newName,
+            id: newName.toLowerCase().replace(/ /g, "-"),
+            colors: this.state.colors
+        };
+        this.props.savePalette(newPalette);
+        this.props.history.push("/")
+    }
+
     render() {
         const { classes} = this.props;
         const { open } = this.state;
@@ -137,12 +149,11 @@ class NewPaletteForm extends Component {
                         [classes.appBarShift]: open,
                     })}
                 >
-                    <Toolbar>
+                    <Toolbar disableGutters={!open}>
                         <IconButton
                             color="inherit"
                             aria-label="open drawer"
                             onClick={this.handleDrawerOpen}
-                            edge="start"
                             className={classNames(classes.menuButton, open && classes.hide)}
                         >
                             <MenuIcon />
@@ -150,6 +161,7 @@ class NewPaletteForm extends Component {
                         <Typography variant="h6" noWrap>
                             Persistent drawer
           </Typography>
+                        <Button variant="contained" color='primary' onClick={this.handleSubmit}>Save Palette</Button>
                     </Toolbar>
                 </AppBar>
                 <Drawer
