6c9574006728babb6411a500f2794e510b426ff5
Nikki <npeel3497@gmail.com>
2020-02-22 18:11:56 -0500

    Updates

---
+++ precache-manifest.e7d35db8d333860c5229f555ac299974.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ec47f9267eaf20282eae4b7f4f696e",
+    "url": "/react-colors-app/index.html"
+  },
+  {
+    "revision": "c1d311f6ea880c054950",
+    "url": "/react-colors-app/static/css/2.c05fe3be.chunk.css"
+  },
+  {
+    "revision": "c6c9492754b5cd5ff5b0",
+    "url": "/react-colors-app/static/css/main.64aef63b.chunk.css"
+  },
+  {
+    "revision": "c1d311f6ea880c054950",
+    "url": "/react-colors-app/static/js/2.71d37db2.chunk.js"
+  },
+  {
+    "revision": "6194c42717583939b4f76332be3503d4",
+    "url": "/react-colors-app/static/js/2.71d37db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c9492754b5cd5ff5b0",
+    "url": "/react-colors-app/static/js/main.5e847e61.chunk.js"
+  },
+  {
+    "revision": "f24080f568953544a5ca",
+    "url": "/react-colors-app/static/js/runtime-main.86550c4c.js"
+  },
+  {
+    "revision": "9b034753c2a22d51a3cc555b23b70959",
+    "url": "/react-colors-app/static/media/bg.9b034753.svg"
+  }
+]);
